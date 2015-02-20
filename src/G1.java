eef35389b40f6009503bd51297f00acfa41e6fc2
lerichard95 <lerichard95@gmail.com>
2015-02-19 22:13:31 -0500

    Setup for Eclipse
    What do all these .jar files mean vs. .class and .java files?

---
+++ src/G1.java
@@ -0,0 +1,9 @@
+public class G1 {
+	
+	/* Main method */
+	public static void main(String[] args) {
+		System.out.println("I sneezed on the beat and");
+		System.out.println("the beat got sicker;");
+	}
+	
+}
