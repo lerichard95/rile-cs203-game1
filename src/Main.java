68053996c5a4067700b2cadc0aa25eb302d130b0
lerichard95 <lerichard95@gmail.com>
2015-03-10 17:18:49 -0400

    first working test
    need to figure out if the tester suites are needed or not

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //This class is specifically for testing— it will run all the tests for the game
+        //  Using the tester library...
         columnsExamples columnsExamples = new columnsExamples();
         Tester.run(columnsExamples);
 
--- src/columnsExamples.java
+++ src/columnsExamples.java
@@ -4,33 +4,23 @@ import javalib.worldimages.Posn;
 
 public class columnsExamples {
 
-    public boolean testsP() {
-        // TODO: Devise tests, write tests...
-        // This function will return false if the tests fail
-        // Each test should be functional
-        // outputs can be verified with code— fully automated testing without
-        // relying on side-effects
-        BlockPosnEquals();
-
-        Posn posn1 = new Posn(10, 10);
-        Pinhole pinHole1 = new Pinhole( posn1 );
-
-        System.out.println(pinHole1);
-
-
-        System.out.println("----------  TESTING COMPLETE!  ----------");
-        return true;
-    }
-
-    public void BlockPosnEquals() {
+    public void testEquals(Tester t) {
 
         Block testBlock1 = new Block(new Posn(100, 100), BlockType.A);
         Block testBlock2 = new Block(new Posn(100, 100), BlockType.A);
-        System.out.println("Should be true || " + "BlockPosnEquals -> " + testBlock1.equals(testBlock1));
+        t.checkExpect(
+                testBlock1.equals(testBlock2),
+                true
+                , "BlockPosnEquals 1 & 2");
 
         Block testBlock3 = new Block(new Posn(100, 100), BlockType.A);
         Block testBlock4 = new Block(new Posn(100, 100), BlockType.B);
-        System.out.println("Should be false || " + "BlockPosnEquals -> " + testBlock3.equals(testBlock4));
+        t.checkExpect(
+                testBlock3.equals(testBlock4),
+                false
+                , "BlockPosnEquals 3 & 4");
+
+
 
     }
 
